32de5134e9c4f7f070d8474657572a96e74d4524
Kira Resari <kira@tri-tail.com>
2021-12-30 17:24:09 +0100

    Added get_children method

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,30 @@ use std::fs;
 mod value_objects;
 use value_objects::*;
 
-pub fn parse_kanji_json(kanji_file_path: &str) -> Result<Vec<ParsedKanjiJsonElement>, Box<dyn Error>>{
-    let contents = fs::read_to_string(kanji_file_path)?;
-    let parsed_kanji: Vec<ParsedKanjiJsonElement> = serde_json::from_str(&contents)?;
-    Ok(parsed_kanji)
+pub struct KanjiParser {
+    parsed_kanji: Vec<ParsedKanjiJsonElement>,
+}
+
+impl KanjiParser{
+
+    pub fn parse_kanji_json(&mut self, kanji_file_path: &str) -> Result<(), Box<dyn Error>>{
+        let contents = fs::read_to_string(kanji_file_path)?;
+        self.parsed_kanji = serde_json::from_str(&contents)?;
+        Ok(())
+    }
+
+    pub fn get_children(&self, identifier: &str) -> Result<Vec<&ParsedKanjiJsonElement>, Box<dyn Error>>{
+        let children: Vec<&ParsedKanjiJsonElement> = self.parsed_kanji.iter()
+            .filter(|x| x.parent_names.contains(&String::from(identifier)))
+            .collect();
+        Ok(children)
+    }
+
+    pub fn new() -> KanjiParser{
+        KanjiParser{
+            parsed_kanji: vec![],
+        }
+    }
 }
 
 #[cfg(test)]
@@ -14,8 +34,38 @@ mod tests {
     use super::*;
 
     #[test]
+    fn get_children_should_return_children(){
+        let mut kanji_parser = KanjiParser::new();
+        kanji_parser.parse_kanji_json("kanji_test_with_three_kanji.json").unwrap();
+
+        let children = kanji_parser.get_children("One").unwrap();
+
+        let kanji1 = ParsedKanjiJsonElement{
+                name: String::from("Two"),
+                node_type: NodeType::Kanji,
+                character: String::from("二"),
+                stroke_arrangement: String::from("2H"),
+                stroke_count: 2,
+                parent_names: vec![String::from("One")]
+            };
+        let kanji2 = ParsedKanjiJsonElement{
+                name: String::from("Three"),
+                node_type: NodeType::Kanji,
+                character: String::from("三"),
+                stroke_arrangement: String::from("3H"),
+                stroke_count: 3,
+                parent_names: vec![String::from("One"), String::from("Two")]
+            };
+
+        let expected_children = vec![&kanji1, &kanji2];
+        assert_eq!(children, expected_children);
+    }
+
+    #[test]
     fn parse_kanji_json_should_not_return_error(){
-        match parse_kanji_json("kanji.json"){
+        let mut kanji_parser = KanjiParser::new();
+
+        match kanji_parser.parse_kanji_json("kanji.json"){
             Ok(_) => println!("Test Passed"),
             Err(error) => panic!("{}", error),
         }
@@ -23,16 +73,18 @@ mod tests {
 
     #[test]
     fn parse_kanji_json_should_return_correct_count_of_kanji(){
-        let parsed_kanji: Vec<ParsedKanjiJsonElement>
-            = parse_kanji_json("kanji_test_with_three_kanji.json").unwrap();
+        let mut kanji_parser = KanjiParser::new();
 
-        assert_eq!(3, parsed_kanji.len());
+        kanji_parser.parse_kanji_json("kanji_test_with_three_kanji.json").unwrap();
+
+        assert_eq!(3, kanji_parser.parsed_kanji.len());
     }
 
     #[test]
     fn parse_kanji_json_should_return_expected_result(){
-        let parsed_kanji: Vec<ParsedKanjiJsonElement>
-            = parse_kanji_json("kanji_test_with_three_kanji.json").unwrap();
+        let mut kanji_parser = KanjiParser::new();
+
+        kanji_parser.parse_kanji_json("kanji_test_with_three_kanji.json").unwrap();
 
         let expected_parsed_kanji_json_elements = vec![
             ParsedKanjiJsonElement{
@@ -61,7 +113,7 @@ mod tests {
             }
         ];
 
-        assert_eq!(expected_parsed_kanji_json_elements, parsed_kanji);
+        assert_eq!(expected_parsed_kanji_json_elements, kanji_parser.parsed_kanji);
     }
 
 }
