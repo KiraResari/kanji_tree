ca865c105653d11569f6fcf07264d21b9c85acfe
Kira Resari <kira@tri-tail.com>
2022-02-16 17:45:23 +0100

    Removed egui
    Revert "Finished Version 0-1-0"
    
    This reverts commit 222c32304afad46a874b8f89c279c967439bad88.

--- src/keys.rs
+++
@@ -1,1 +0,0 @@
-pub const FONT_GOTHIC: &str = "gothic";
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
 fn main() {
-    let app = kanji_tree::KanjiTreeApp::default();
-    let native_options = eframe::NativeOptions::default();
-    eframe::run_native(Box::new(app), native_options);
+    println!("Hello, world!");
 }
