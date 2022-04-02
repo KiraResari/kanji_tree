c8604c48394e6ec141321fadf36feb3bc98ac905
Kira Resari <kira@tri-tail.com>
2022-04-02 11:36:05 +0200

    Added working app icon

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
 use iced::{
     Settings, Application, window::Icon, 
 };
+
+static ICON: &[u8]
+     = include_bytes!("../resources/images/Kanji Tree R Icon.png");
+const ICON_HEIGHT: u32 = 512;
+const ICON_WIDTH: u32 = 512;
+
 pub fn main() -> iced::Result {
-    let mut settings = Settings::default();
-    settings.window.icon =  Icon::from_rgba(
-        include_bytes!(
-            "../resources/images/Kanji Tree R Icon.ico"
-        ).to_vec(),
-        512,
-        512
-    ).ok();
+    let settings = build_settings();
     kanji_tree::KanjiTreeApp::run(settings)
+}
+
+fn build_settings() -> Settings<()> {
+    let image
+        = image::load_from_memory(ICON).unwrap();
+    let icon = Icon::from_rgba(
+        image.as_bytes().to_vec(),
+        ICON_HEIGHT,
+        ICON_WIDTH
+    ).unwrap();
+    Settings {
+        window: iced::window::Settings {
+            icon: Some(icon), 
+            ..Default::default()
+        },
+        ..Default::default()
+    }
 }
